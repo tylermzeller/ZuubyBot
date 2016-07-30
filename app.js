08b7506f400a51d7ef61b6644fc6aa64f321241d
tyzeller <tyzeller@cisco.com>
2016-07-30 14:34:04 -0700

    Add db folder. Remove schema.sql.

--- app.js
+++ app.js
@@ -20,8 +20,18 @@ var client = new tmi.client(options);
 client.connect();
 
 client.on('chat', function(channel, user, message, self) {
-	if (message.match(/zuubybot/i) || message.match(/zuuby/i)){
-		client.action(channel, 'Hello ' + user['display-name'] === null ? user['username'] : user['display-name']);
+	if (message.match(/zuubybot/i) || message.match(/zuuby/i)) {
+		client.action(channel, 'Hello ' + (user['display-name'] === null ? user['username'] : user['display-name']) + '!');
+	} else if (message === '!game') { // TODO: check if a user is already playing a game
+
+	} else if (message === '!resign') { //TODO: check if a user is actually playing
+
+	} else if (message === '!draw') { // TODO: check if user is playing, check if score is close to 0.0 or losing
+
+	} else if (message === '!score') { // TODO check if user is playing
+
+	} else if (message === '!bestmove') { // TODO: check if user is playing
+
 	}
 });
 
